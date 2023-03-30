fed1ed33ccbdc4844f480ee7f5f0825461ee3ca6
Tyler Flaherty <tylerjf04@gmail.com>
2023-03-30 17:00:18 -0400

    Added some fixes to the stat classes
    - Modified percentage based modfiers
      to allow for both flat percentage
      added as well as percentage
      multipliers
    - Added a source variable to the
      StatModifier enum to show what
      item the modifier was coming from
    - Added a list that allows users to see
      where their modifiers were coming
      from
    - Additional minor changes

--- Bounties and Blades/Assets/CharacterStats.cs
+++ Bounties and Blades/Assets/CharacterStats.cs
@@ -5,14 +5,17 @@ Sources Sited:
 
 using System;
 using System.Collections.Generic;
+using UnityEngine.UIElements;
+using System.Collections.ObjectModel;
 
 public class CharacterStats
 {
     public float BaseValue;
 
     public float TotalValue { get {
-            if (needsUpdate)
+            if (needsUpdate || BaseValue != lastBaseValue)
             {
+                lastBaseValue = BaseValue;
                 _value = CalculateFinalValue();
                 needsUpdate = false;
             }
@@ -21,13 +24,16 @@ public class CharacterStats
 
     private bool needsUpdate = true;
     private float _value;
+    private float lastBaseValue = float.MinValue;
 
     private readonly List<StatModifier> statModifiers;
+    public readonly ReadOnlyCollection<StatModifier> StatModifiers; // Allows players to see their stat modifiers
 
     public CharacterStats(float baseValue)
     {
         BaseValue = baseValue;
         statModifiers = new List<StatModifier>();
+        StatModifiers = statModifiers.AsReadOnly();
     }
 
     public void AddModifier(StatModifier modifier)
@@ -44,15 +50,35 @@ public class CharacterStats
         return 0;
     }
 
-    public void RemoveModifier(StatModifier modifier)
+    public bool RemoveModifier(StatModifier modifier)
     {
-        needsUpdate = true;
-        statModifiers.Remove(modifier);
+        if (statModifiers.Remove(modifier))
+        {
+            needsUpdate = true;
+            return true;
+        }
+        return false;
+    }
+
+    public bool RemoveAllModifiersFromSource(object source)
+    {
+        bool didRemove = false;
+        for (int i = statModifiers.Count - 1; i >= 0; i++)
+        {
+            if (statModifiers[i].Source == source)
+            {
+                needsUpdate = true;
+                didRemove = true;
+                statModifiers.RemoveAt(i);
+            }
+        }
+        return didRemove;
     }
 
     private float CalculateFinalValue()
     {
         float finalValue = BaseValue;
+        float sumPercentAdd = 0;
 
         for (int i = 0; i < statModifiers.Count; i++)
         {
@@ -62,7 +88,17 @@ public class CharacterStats
             {
                 finalValue += statModifiers[i].Value; // For flat modifiers simply add them to the base stats
             }
-            else if (modifier.Type == StatModType.Percent)
+            else if (modifier.Type == StatModType.PercentAdd)
+            {
+                sumPercentAdd += modifier.Value;
+
+                if (i + 1 >= statModifiers.Count || statModifiers[i + 1].Type != StatModType.PercentAdd)
+                {
+                    finalValue *= 1 + sumPercentAdd;
+                    sumPercentAdd = 0;
+                }
+            }
+            else if (modifier.Type == StatModType.PercentMult)
             {
                 finalValue *= 1 + modifier.Value;
             }
