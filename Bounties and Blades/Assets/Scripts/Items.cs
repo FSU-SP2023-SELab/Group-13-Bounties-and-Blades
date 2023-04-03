b086aa9f9f584014731f74723ffb068026ab6715
Tyler Flaherty <tylerjf04@gmail.com>
2023-04-03 12:58:59 -0400

    Updated from main

---
+++ Bounties and Blades/Assets/Scripts/Items.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items
+{
+    public string itemName;
+    public string itemDescription;
+    public Sprite icon;
+     
+}
