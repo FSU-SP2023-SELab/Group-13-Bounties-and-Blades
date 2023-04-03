0b056f8b747399084921cdeda20be9a05afab01c
Tyler Flaherty <tylerjf04@gmail.com>
2023-04-03 13:05:06 -0400

    Resolved a compilation error
    The class "Items" was causing a compilation error due to there already being an "Items" class in the global scope, so I renamed the class "CharacterItems" to resolve this

--- Bounties and Blades/Assets/Scripts/CharacterItems.cs
+++ Bounties and Blades/Assets/Scripts/CharacterItems.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Items
+public class CharacterItems
 {
     public string itemName;
     public string itemDescription;
