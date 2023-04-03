d13aa855ba7a25b5a398bf690c2adcf183e28b72
jiksuthejedi <43581775+jiksuthejedi@users.noreply.github.com>
2023-04-03 11:52:32 -0400

    Character Class and Stats
    Base character class created. To be updated and added on top of. Specific classes in the game will exist as children of this class. Character stats basic implementation in character class.

---
+++ Bounties and Blades/Assets/Scripts/CharacterClass.cs
@@ -0,0 +1,85 @@
+using BountiesAndBlades.CharacterStats;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using UnityEngine;
+
+public class CharacterClass
+{
+    private string className;
+    private string classDescription;
+
+    private int HP;
+    private int Armor;
+    private int[] StatsList  = new int[6]; //0 Strength, 1 Speed, 2 Defense, 3 Intelligence, 4 Constitution, 5 Luck
+
+    private CharacterStats modifiers = new CharacterStats();
+    public string getName()
+    {
+        return className;
+    }
+    public string getDescription()
+    {
+        return classDescription;
+    }
+    public int getHP()
+    { 
+        return HP;
+    }
+    public int getArmor()
+    { 
+        return Armor;
+    }
+    public int getStat(int i)
+    { 
+        return StatsList[i];
+    }
+    public int[] getAllStats()
+    {
+        return StatsList;
+    }
+
+    public string[,] getModifiers()
+    {
+        string[,] r = new string[2, modifiers.StatModifiers.Count];
+
+        for (int i = 0; i < modifiers.StatModifiers.Count; i++)
+        {
+            r[1, i] = modifiers.StatModifiers[i].Value.ToString();
+            r[0, i] = modifiers.StatModifiers[i].Type.ToString();
+        }
+        
+        return r;
+    }
+
+    public void setName(string n)
+    {
+        className = n;
+    }
+
+    public void setDescription(string s)
+    {
+        classDescription = s;
+    }
+    public void setHP(int i)
+    {
+        HP = i;
+    }
+    public void setArmor(int i)
+    {
+
+        Armor = i;
+    }
+
+    public void setStat(int i, int v)
+    { 
+     StatsList[i] = v;
+    }
+
+    public void addModifier(StatModifier s)
+    { 
+        modifiers.AddModifier(s);
+    }
+    
+}
---
+++ Bounties and Blades/Assets/Scripts/TestGUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGUI : MonoBehaviour
+{
+    private CharacterClass archer = new ArcherClass();
+    private CharacterClass archer2 = new ArcherClass();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        string[,] mods = archer.getModifiers();
+        GUILayout.Label(archer.getName());
+        int modLength = mods.GetLength(1);
+        for (int i = 0; i < modLength; i++)
+        {
+            GUILayout.Label(archer.getModifiers()[0, i] +", "+ archer.getModifiers()[1, i]);
+        }
+        archer2.addModifier(new BountiesAndBlades.CharacterStats.StatModifier(100, new BountiesAndBlades.CharacterStats.StatModType(), 1, 0));
+        GUILayout.Label(archer2.getName());
+        mods = archer2.getModifiers();
+        modLength = mods.GetLength(1);
+        for (int i = 0; i < modLength; i++)
+        {
+            GUILayout.Label(archer2.getModifiers()[0, i] + ", " + archer2.getModifiers()[1, i]);
+        }
+    }
+}
