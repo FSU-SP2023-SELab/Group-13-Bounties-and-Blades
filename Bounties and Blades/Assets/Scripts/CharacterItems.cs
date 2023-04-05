ab5810052e7bb9a20b95bfead747dfba13e5a5be
Tyler Flaherty <tylerjf04@gmail.com>
2023-04-05 13:00:59 -0400

    Added blank classes for consumables, weapons, and armor

--- Bounties and Blades/Assets/Scripts/CharacterItems.cs
+++ Bounties and Blades/Assets/Scripts/CharacterItems.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BountiesAndBlades.CharacterClass;
 
 public class CharacterItems
 {
     public string itemName;
     public string itemDescription;
     public Sprite icon;
+    public int maxStackSize;
+
+    public virtual void Use(CharacterClass characterClass)
+    {
+
+    }
+
+    public class Consumable
+    {
+
+    }
+
+    public class Weapon
+    {
+
+    }
+
+    public class Armor
+    {
+
+    }
      
 }
