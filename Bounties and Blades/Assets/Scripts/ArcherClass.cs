e3cc072bab4cd80e1d4ab0bb3141c8fef220a65e
tc19a <tc19a@fsu.edu>
2023-04-19 12:22:14 -0400

    Fixed Null reference exception with tiles

--- Bounties and Blades/Assets/Scripts/ArcherClass.cs
+++ Bounties and Blades/Assets/Scripts/ArcherClass.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArcherClass : CharacterClass
+namespace BountiesAndBlades.CharacterClass
 {
-    public ArcherClass()
+    public class ArcherClass : CharacterClass
     {
-        setName("Archer");
-        setDescription("Swift and great from long ditances. The archer carries a bow and wears leather armor");
-        setHP(10);
-        setArmor(0);
-        setStat(0, 4);
-        setStat(1, 7);
-        setStat(2, 3);
-        setStat(3, 6);
-        setStat(4, 6);
-        setStat(5, 3);
+        public ArcherClass()
+        {
+            setName("Archer");
+            setDescription("Swift and great from long ditances. The archer carries a bow and wears leather armor");
+            setHP(10);
+            setArmor(0);
+            setStat(0, 4);
+            setStat(1, 7);
+            setStat(2, 3);
+            setStat(3, 6);
+            setStat(4, 6);
+            setStat(5, 3);
+        }
     }
+
 }
