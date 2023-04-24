9bab92c66332d5195210dfad37a79b5f1370cc90
Tyler Flaherty <tylerjf04@gmail.com>
2023-04-24 14:31:09 -0400

    Further updates to the inventory system

---
+++ Bounties and Blades/Assets/Scenes/InventorySystem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BountiesAndBlades.BaseHero;
+using BountiesAndBlades.CharacterItems;
+using BountiesAndBlades.CharacterClass;
+
+public class InventorySystem : MonoBehaviour
+{
+    public List<CharacterItems> items = new List<CharacterItems>();
+
+    public virtual void RemoveItem(CharacterItems item)
+    {
+        items.Remove(item);
+    }
+
+    public virtual void EquipItem(CharacterItems item)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
