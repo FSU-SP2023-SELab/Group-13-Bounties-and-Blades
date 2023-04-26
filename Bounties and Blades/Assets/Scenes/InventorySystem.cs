3f37a8fce436896b38880299e21856807c76cdf4
Tyler Flaherty <tylerjf04@gmail.com>
2023-04-26 16:02:23 -0400

    Updated CharacterItems

--- Bounties and Blades/Assets/Scenes/InventorySystem.cs
+++ Bounties and Blades/Assets/Scenes/InventorySystem.cs
@@ -1,32 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using BountiesAndBlades.BaseHero;
-using BountiesAndBlades.CharacterItems;
-using BountiesAndBlades.CharacterClass;
-
-public class InventorySystem : MonoBehaviour
+namespace BountiesAndBlades.InventorySystem
 {
-    public List<CharacterItems> items = new List<CharacterItems>();
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using BountiesAndBlades.BaseHero;
+    using BountiesAndBlades.CharacterItems;
+    using BountiesAndBlades.CharacterClass;
 
-    public virtual void RemoveItem(CharacterItems item)
-    {
-        items.Remove(item);
-    }
 
-    public virtual void EquipItem(CharacterItems item)
-    {
 
-    }
-    // Start is called before the first frame update
-    void Start()
+    public class InventorySystem : MonoBehaviour
     {
-        
-    }
+        public List<CharacterItems> items = new List<CharacterItems>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public virtual void RemoveItem(CharacterItems item)
+        {
+            items.Remove(item);
+        }
+
+        public virtual void EquipItem(CharacterItems item)
+        {
+
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
