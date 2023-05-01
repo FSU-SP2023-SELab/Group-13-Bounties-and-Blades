c2373052c8d1bd1b9023f4cfb665c290318ad2d9
Tyler Flaherty <tylerjf04@gmail.com>
2023-05-01 14:04:09 -0400

    Changed BaseHero to use CHaracterStatList for stats

--- Bounties and Blades/Assets/Scripts/Units/BaseHero.cs
+++ Bounties and Blades/Assets/Scripts/Units/BaseHero.cs
@@ -49,7 +49,7 @@ namespace BountiesAndBlades.BaseHero {
         }
         public double getStat(int i)
         {
-            return StatsList[i];
+            return CharacterStatList[i].TotalValue;
         }
         public double[] getAllStats()
         {
@@ -121,11 +121,11 @@ namespace BountiesAndBlades.BaseHero {
             if(dmg == 0){
                 return false;
             }
-            if(dmg - (int)StatsList[2]/2.5 <= 0){
+            if(dmg - (int)getStat(2)/2.5 <= 0){
                 HP -= 1;
             }
             else{
-                HP -= (int)(dmg - StatsList[2]/2.5); //armor mitigated the damage
+                HP -= (int)(dmg - getStat(2)/2.5); //armor mitigated the damage
             }
             if (HP <= 0){
                 return true;
